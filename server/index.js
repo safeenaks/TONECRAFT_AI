a5e593dafcfc6ebce9d43f69111aaf6341ee6384
Dinesync <shanavaskh123456@gmail.com>
2026-06-16 18:01:24 +0530

    const port added

--- server/index.js
+++ server/index.js
@@ -31,6 +31,8 @@ const groq = new Groq({
    ROUTE
 ========================= */
 
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
